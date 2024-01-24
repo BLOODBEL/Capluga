612ee6b4af1383dd85c8dfc4eb6b10d7ff610619
arielguido <79162870+arielguido@users.noreply.github.com>
2024-01-24 16:56:43 -0600

    Vistas

---
+++ Capluga/Capluga/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capluga.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult About()
+        {
+          
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Contact()
+        {
+           
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Productos()
+        {
+
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult services()
+        {
+
+
+            return View();
+        }
+    }
+}
