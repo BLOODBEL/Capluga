e445e5891f9fb491b648b277993a1dcb59f6245f
arielguido <79162870+arielguido@users.noreply.github.com>
2024-02-14 21:01:43 -0600

    API
    API de las partes de agendado de citas y registros de horarios.

---
+++ CaplugaAPI/CaplugaAPI/Controllers/CitaHorarioController.cs
@@ -0,0 +1,124 @@
+﻿using CaplugaAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CaplugaAPI.Controllers
+{
+    public class CitaHorarioController : ApiController
+    {
+
+
+        [HttpGet]
+        [Route("ConsultaHorarios")]
+        public List<ScheduleAppointment> ConsultaHorarios()
+        {
+            try
+            {
+                using (var context = new CAPLUGAEntities())
+                {
+                    context.Configuration.LazyLoadingEnabled = false;
+                    return (from x in context.ScheduleAppointment
+                            select x).ToList();
+                }
+            }
+            catch (Exception)
+            {
+                return new List<ScheduleAppointment>();
+            }
+        }
+
+        [HttpGet]
+        [Route("ConsultaHorario")]
+        public ScheduleAppointment ConsultaHorario(long q)
+        {
+            try
+            {
+                using (var context = new CAPLUGAEntities())
+                {
+                    context.Configuration.LazyLoadingEnabled = false;
+                    return (from x in context.ScheduleAppointment
+                            where x.ScheduleID == q
+                            select x).FirstOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        [HttpPost]
+        [Route("RegistrarHorarios")]
+        public string RegistrarHorarios(HorarioEnt entidad)
+        {
+            try
+            {
+                using (var context = new CAPLUGAEntities())
+                {
+                    context.InsertScheduleAppointment(entidad.Dname, entidad.DateandTime);
+                    return "OK";
+                }
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+
+
+        [HttpPut]
+        [Route("ActualizarHorarios")]
+        public string ActualizarHorarios(HorarioEnt entidad)
+        {
+            try
+            {
+                using (var context = new CAPLUGAEntities())
+                {
+                    context.UpdateScheduleAppointment(entidad.ScheduleID, entidad.Dname, entidad.DateandTime);
+                    return "OK";
+                }
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+
+          [HttpGet]
+        [Route("verHorarios")]
+        public List<System.Web.Mvc.SelectListItem> verHorarios()
+        {
+            try
+            {
+                using (var context = new CAPLUGAEntities())
+                {
+                    var datos = (from x in context.ScheduleAppointment
+                                 select x).ToList();
+
+                    List<System.Web.Mvc.SelectListItem> horarios = new List<System.Web.Mvc.SelectListItem>();
+                    foreach (var item in datos)
+                    {
+                        string displayText = $"{item.Dname} - {item.DateandTime.ToString("dd/MM/yyyy HH:mm")}"; 
+
+                        horarios.Add(new System.Web.Mvc.SelectListItem
+                        {
+                            Value = item.ScheduleID.ToString(),
+                            Text = displayText
+                          
+                        });
+                    }
+
+                    return horarios;
+                }
+            }
+            catch (Exception)
+            {
+                return new List<System.Web.Mvc.SelectListItem>();
+            }
+        }
+    }
+}
---
+++ CaplugaAPI/CaplugaAPI/Entities/HorarioEnt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CaplugaAPI.Entities
+{
+    public class HorarioEnt
+    {
+
+        public long ScheduleID { get; set; }
+
+        public string Dname { get; set; }
+
+        public DateTime DateandTime { get; set;}
+    }
+}
