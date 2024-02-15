4d6dd73c33dd96d6a2714bd2d736bee760abe8e3
arielguido <79162870+arielguido@users.noreply.github.com>
2024-02-14 21:01:43 -0600

    API
    API de las partes de agendado de citas y registros de horarios.

---
+++ CaplugaAPI/CaplugaAPI/Entities/AgendaEnt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaplugaAPI.Entities
+{
+    public class AgendaEnt
+    {
+        public long AppointmentID { get; set; }
+
+        public long UserID { get; set; }
+
+            public long ScheduleID { get; set; }
+
+            public long AddressID { get; set; }
+
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+
+        
+    }
+
+    }
