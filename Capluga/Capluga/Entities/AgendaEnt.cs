68f47979a790475a5884d919a05be0f428ddfc91
arielguido <79162870+arielguido@users.noreply.github.com>
2024-02-14 19:55:27 -0600

    la crecion de los crud de Horarios y agendado de citas y vistas requeridas

---
+++ Capluga/Capluga/Entities/AgendaEnt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capluga.Entities
+{
+    public class AgendaEnt
+    {
+
+        public long AppointmentID { get; set; }
+
+        public long UserID { get; set; }
+
+        public long AddressID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public long ScheduleID { get; set; }
+    }
+}
---
+++ Capluga/Capluga/Models/AgendaCitaModel.cs
@@ -0,0 +1,61 @@
+﻿using Capluga.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capluga.Models
+{
+    public class AgendaCitaModel
+    {
+        public string rutaServidor = ConfigurationManager.AppSettings["RutaApi"];
+
+
+     
+        public string RegistrarCita(AgendaEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                var urlApi = rutaServidor + "RegistrarCita";
+                var jsonData = JsonContent.Create(entidad);
+                var res = client.PostAsync(urlApi, jsonData).Result;
+                return res.Content.ReadFromJsonAsync<string>().Result;
+            }
+        }
+
+        public List<AgendaEnt> ConsultaCitas()
+        {
+            using (var client = new HttpClient())
+            {
+                var urlApi = rutaServidor + "ConsultaCitas";
+                var res = client.GetAsync(urlApi).Result;
+                return res.Content.ReadFromJsonAsync<List<AgendaEnt>>().Result;
+            }
+        }
+
+        public AgendaEnt ConsultaCita(long q)
+        {
+            using (var client = new HttpClient())
+            {
+                var urlApi = rutaServidor + "ConsultaCita?q=" + q;
+                var res = client.GetAsync(urlApi).Result;
+                return res.Content.ReadFromJsonAsync<AgendaEnt>().Result;
+            }
+        }
+        public string Actualizarcita(AgendaEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                var urlApi = rutaServidor + "Actualizarcita";
+                var jsonData = JsonContent.Create(entidad);
+                var res = client.PutAsync(urlApi, jsonData).Result;
+                return res.Content.ReadFromJsonAsync<string>().Result;
+            }
+        }
+
+    }
+}
