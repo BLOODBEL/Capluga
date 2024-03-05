9685f7c885274ed91774596902e0001aef16cc6d
BLOODBEL <101367583+BLOODBEL@users.noreply.github.com>
2024-03-04 22:43:27 -0600

    api

---
+++ CaplugaAPI/CaplugaAPI/Controllers/ProductoController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaplugaAPI.Controllers
+{
+    public class ProductoController : ApiController
+    {
+        [HttpGet]
+        [Route("Productos")]
+        public List<MedicalImplements> Productos()
+        {
+            using (var context = new CAPLUGAEntities())
+            {
+                context.Configuration.LazyLoadingEnabled = false;
+                return context.MedicalImplements.ToList();
+            }
+        }
+
+        [HttpGet]
+        [Route("Producto")]
+        public MedicalImplements Producto(long q)
+        {
+            using (var context = new CAPLUGAEntities())
+            {
+                context.Configuration.LazyLoadingEnabled = false;
+                return (from x in context.MedicalImplements
+                        where x.MedicalImplementsID == q
+                        select x).FirstOrDefault();
+            }
+        }
+
+        [HttpPost]
+        [Route("RegistrarProducto")]
+        public long RegistrarProducto(MedicalImplements MedicalImplements)
+        {
+            using (var context = new CAPLUGAEntities())
+            {
+                context.MedicalImplements.Add(MedicalImplements);
+                context.SaveChanges();
+                return MedicalImplements.MedicalImplementsID;
+            }
+        }
+
+        [HttpPut]
+        [Route("ActualizarRutaProducto")]
+        public string ActualizarRutaProducto(MedicalImplements MedicalImplements)
+        {
+            using (var context = new CAPLUGAEntities())
+            {
+                var datos = context.MedicalImplements.Where(x => x.MedicalImplementsID == MedicalImplements.MedicalImplementsID).FirstOrDefault();
+                datos.Image = MedicalImplements.Image;
+                context.SaveChanges();
+                return "OK";
+            }
+        }
+
+
+
+
+
+        [HttpPut]
+        [Route("ActualizarProducto")]
+        public string ActualizarProducto(MedicalImplements MedicalImplements)
+        {
+            using (var context = new CAPLUGAEntities())
+            {
+                var datos = context.MedicalImplements.Where(x => x.MedicalImplementsID == MedicalImplements.MedicalImplementsID).FirstOrDefault();
+                datos.Name = MedicalImplements.Name;
+                datos.Description = MedicalImplements.Description;
+                datos.Quantity = MedicalImplements.Quantity;
+                datos.Price = MedicalImplements.Price;
+                context.SaveChanges();
+                return "OK";
+            }
+        }
+
+
+
+        [HttpPut]
+        [Route("ActualizarEstadoProducto")]
+        public string ActualizarEstadoUsuario(MedicalImplements MedicalImplements)
+        {
+            using (var context = new CAPLUGAEntities())
+            {
+                var datos = context.MedicalImplements.Where(x => x.MedicalImplementsID == MedicalImplements.MedicalImplementsID).FirstOrDefault();
+                datos.State = (datos.State ? false : true);
+                context.SaveChanges();
+                return "OK";
+            }
+        }
+    }
+}
---
+++ CaplugaAPI/CaplugaAPI/Entities/CarritoEnt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CaplugaAPI.Entities
+{
+    public class CarritoEnt
+    {
+
+        public long CartID { get; set; }
+
+        public long UserID { get; set; }
+
+        public long MasterPurchaseID { get; set; }
+
+        public long MedicalImplementsID { get; set; }
+
+        public decimal Quantity { get; set; }
+
+
+
+    }
+}
